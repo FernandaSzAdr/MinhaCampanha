ae886faf782feb68c1d8bc6cbc6bca1e6bdb67c5
Fernanda Souza <fsandrade25@gmail.com>
2018-01-21 20:45:58 -0300

    mudança no pom

---
+++ src/main/java/br/ufrpe/minhacampanha/bean/LoginBean.java
@@ -0,0 +1,23 @@
+package br.ufrpe.minhacampanha.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+
+@ManagedBean 
+public class LoginBean {
+	
+	public void logar(){
+		
+		/*String texto = "Login realizado com sucesso!";
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+		
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem);*/
+	}
+	
+	public void criarConta(){
+		
+	}
+}
