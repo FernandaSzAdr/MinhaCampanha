469f1b1eafa96f4185b447d80e033ae0b25e918a
Jonathan Oliveira <jonathancgo94@gmail.com>
2018-01-29 14:48:28 -0300

    classe teste

---
+++ src/main/java/br/ufrpe/minhacampanha/test/TestConnection.java
@@ -0,0 +1,27 @@
+package br.ufrpe.minhacampanha.test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Test;
+import org.omnifaces.util.Messages;
+
+import br.ufrpe.minhacampanha.dao.InstituicaoDAO;
+import br.ufrpe.minhacampanha.domain.Instituicao;
+
+
+
+public class TestConnection {
+
+	@Test
+	public void listar() throws SQLException{
+		List<Instituicao> instituicoes;
+		try {
+			InstituicaoDAO instituicaoDAO = new InstituicaoDAO();
+			instituicoes = instituicaoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as instituições.");
+			erro.printStackTrace();
+		}
+	}
+}
