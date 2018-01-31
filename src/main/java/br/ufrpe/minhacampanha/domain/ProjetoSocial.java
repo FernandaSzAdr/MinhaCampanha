f1c6de9dec866786cc17363f01953f3328e746aa
Srta Camelo <raissa_camelo@hotmail.com>
2018-01-31 20:08:47 -0300

    Projeto Social, DAO

--- src/main/java/br/ufrpe/minhacampanha/domain/ProjetoSocial.java
+++ src/main/java/br/ufrpe/minhacampanha/domain/ProjetoSocial.java
@@ -2,10 +2,21 @@ package br.ufrpe.minhacampanha.domain;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author raiss
+ *
+ */
 public class ProjetoSocial extends GenericDomain {
 	private String tipo, nome, descricao, objetivo;
 	private LocalDate data_inicio, data_fim;
+	private int qtd_volunt_atual;
+	private int qtd_volunt_nec;
+	private int id_inst; //REFERENCIA instituição
+	
+	public ProjetoSocial() {
+		
+	}
 	
 	public ProjetoSocial(String tipo, String nome, String descricao, String objetivo, LocalDate data_inicio,
 			LocalDate data_fim) {
@@ -68,4 +79,33 @@ public class ProjetoSocial extends GenericDomain {
 	public void setData_fim(LocalDate data_fim) {
 		this.data_fim = data_fim;
 	}
+
+	public int getQtd_volunt_atual() {
+		return qtd_volunt_atual;
+	}
+
+	public void setQtd_volunt_atual(int qtd_volunt_atual) {
+		this.qtd_volunt_atual = qtd_volunt_atual;
+	}
+
+	public int getQtd_volunt_nec() {
+		return qtd_volunt_nec;
+	}
+
+	public void setQtd_volunt_nec(int qtd_volunt_nec) {
+		this.qtd_volunt_nec = qtd_volunt_nec;
+	}
+
+	public int getId_inst() {
+		return id_inst;
+	}
+
+	public void setId_inst(int id_inst) {
+		this.id_inst = id_inst;
+	}
+
+	public void setData_inicio(LocalDate data_inicio) {
+		this.data_inicio = data_inicio;
+	}
+	
 }
